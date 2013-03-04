0e02395db119b5c9d9ce9771e6068d5ede275f66
lhobbs <elisa.hobbs@colorado.edu>
2013-03-04 16:53:22 -0700

    Update Lab3.scala

--- Lab3/src/main/scala/Lab3.scala
+++ Lab3/src/main/scala/Lab3.scala
@@ -6,7 +6,7 @@ object Lab3 {
    * <Lisa Hobbs>
    * 
    * Partner: <Your Partner's Name>
-   * Collaborators: <Any Collaborators>
+   * Collaborators: <Daniel Morrissey, Josh Rahm>
    */
 
   /*
@@ -157,8 +157,8 @@ object Lab3 {
       
       /* I added this function, it doesn't work totally yet though :( */
       case Call(e1, e2) => (eval(env,e1), e2) match{
-        case (Function(None, x, e1), e2) => val env2 = extend(env, x, eval(env,e2)); eval(env2, e1) //?
-        case (Function(Some(x1), x2, ex), e2) =>  val env2 = extend(env, x2, eval(env,e1)); val env3 = extend(env2, x2, eval(env,e2)); eval(env3, ex);
+        case (Function(None, x, e1), e2) => val env2 = extend(env, x, eval(env,e2)); eval(env2, e1) 
+        case (Function(Some(x1), x2, ex), e2) =>  val env2 = extend(env, x1, eval(env,e1)); val env3 = extend(env2, x2, eval(env,e2)); eval(env3, ex);
         case _ => throw new DynamicTypeError(e)
       }
         
@@ -190,19 +190,15 @@ object Lab3 {
       case Print(e1) => Print(subst(e1))
       case Binary(bop,e1,e2) => Binary(bop, subst(e1), subst(e2))
       case Unary(uop, e1) => Unary(uop, subst(e1))
-     
       case Var(y) => if (x==y) {v} else { e }
-
-      case ConstDecl(y, e1, e2) => if (x==y){ ConstDecl(x, subst(e1), e2) } else{ ConstDecl(y,subst(e1),subst(e2)) }
-      
+      case ConstDecl(y, e1, e2) => if (x==y){ ConstDecl(x, subst(e1), e2) } else{ ConstDecl(y,subst(e1),subst(e2)) }  
       case Call(e1, e2) => Call(subst(e1), subst(e2))
-    
+      case If(e1, e2, e3) => If(subst(e1), subst(e2), subst(e3))
+      
       case Function(a, x1, e1) if(x == x1) => e
       case Function(Some(x1), x2, e1) if (x1 == x) => e
       case Function(a, x1, e1) => Function(a, x1, subst(e1))
-      
-      case If(e1, e2, e3) => If(subst(e1), e2, e3)
-      
+
       case _ => throw new DynamicTypeError(e)
     }
   }
@@ -212,7 +208,6 @@ object Lab3 {
     e match {
       /* Base Cases: Do Rules */
       case Print(v1) if (isValue(v1)) => println(pretty(v1)); Undefined
-
 
       case Unary(uop, e1) if (isValue(e1)) => uop match{
         case Neg => N(-toNumber(e1))
@@ -245,8 +240,7 @@ object Lab3 {
         case (v1, S(s2)) => B(toString(v1) < s2)
         case (v1, v2) => B(toNumber(e1) < toNumber(e2))
       }
-       
-      
+ 
       case Binary(Le, e1, e2) if (isValue(e1) && isValue(e2)) => (e1, e2) match{
         case(S(s1), v2) => B(s1 <= toString(v2))
         case (v1, S(s2)) => B(toString(v1) <= s2)
@@ -254,19 +248,17 @@ object Lab3 {
       }
       
       case Binary(Gt, e1, e2) if (isValue(e1) && isValue(e2)) => (e1, e2) match{
-        case(S(s1), v2) => B(s1 > toString(v2))
+        case (S(s1), v2) => B(s1 > toString(v2))
         case (v1, S(s2)) => B(toString(v1) > s2)
         case (v1, v2) => B(toNumber(e1) > toNumber(e2))
       }
-      
-      
+
       case Binary(Ge, e1, e2) if (isValue(e1) && isValue(e2)) => (e1, e2) match{
         case(S(s1), v2) => B(s1 >= toString(v2))
         case (v1, S(s2)) => B(toString(v1) >= s2)
         case (v1, v2) => B(toNumber(e1) >= toNumber(e2))
       }
-        
-      
+  
       case Binary(And, e1, e2) if (isValue(e1)) => B(toBoolean(e1)) match{
         case B(true) => e2
         case _ => B(false)
@@ -287,7 +279,7 @@ object Lab3 {
 
      case Call(e1, e2) if( isValue(e1) && isValue(e2)) => (e1, e2) match{
         case (Function(None, x, e1), e2) => substitute(e1, e2, x)
-        case (Function(Some(x1), x2, ex), e2) if isValue(e1) => substitute(e2, e1, x1) 
+        case (Function(Some(x1), x2, ex), e2) if isValue(e1) => substitute(substitute(ex, e1, x1), e2, x2)
         case _ => throw new DynamicTypeError(e)
       }
       
@@ -298,13 +290,13 @@ object Lab3 {
         case Neg => Unary(Neg, step(e1))
         case Not => Unary(Not, step(e1))
       }
-      
+
       case Binary(Plus, e1, e2) if !isValue(e1) => Binary(Plus, step(e1), e2)
       case Binary(Minus, e1, e2) if !isValue(e1) => Binary(Minus, step(e1), e2)
       case Binary(Times, e1, e2) if !isValue(e1) => Binary(Times, step(e1), e2)
       case Binary(Div, e1, e2) if !isValue(e1) => Binary(Div, step(e1), e2)
-
-      case Binary(Plus, e1, e2) if !isValue(e2) => Binary(Plus, e1, step(e2))     
+      
+      case Binary(Plus, e1, e2) if !isValue(e2) => Binary(Plus, e1, step(e2))      
       case Binary(Minus, e1, e2) if !isValue(e2) => Binary(Minus, e1, step(e2))
       case Binary(Times, e1, e2) if !isValue(e2) => Binary(Times, e1, step(e2))
       case Binary(Div, e1, e2) if !isValue(e2) => Binary(Div, e1, step(e2))
@@ -334,26 +326,35 @@ object Lab3 {
       case Binary(Gt, e1, e2) if !isValue(e2) => Binary(Gt, e1, step(e2))
       
       case Binary(Ge, e1, e2) if !isValue(e2) => Binary(Ge, e1, step(e2))
-       
-      case Binary(And, e1, e2) => Binary(And, step(e1), e2)
+
+      case Binary(And, e1, e2) if !isValue(e1) => Binary(And, step(e1), e2)
       
-      case Binary(Or, e1, e2) => Binary(Or, step(e1), e2)
+      case Binary(Or, e1, e2) if !isValue(e1) => Binary(Or, step(e1), e2)
       
-      case Binary(Seq, e1, e2) => Binary(Seq, step(e1), e2)
+      case Binary(Seq, e1, e2) if !isValue(e1) => Binary(Seq, step(e1), e2)
       
-      case If(e1, e2, e3) => If(step(e1), e2, e3)
+      case If(e1, e2, e3) if !isValue(e1) => If(step(e1), e2, e3)
       
-      case ConstDecl(x, e1, e2) if (!isValue(e1))=> ConstDecl(x, step(e1), e2)
+      case ConstDecl(x, e1, e2) if (!isValue(e1)) => ConstDecl(x, step(e1), e2)
+
+      
       
       case Call(e1, e2) if !isValue(e1) => Call(step(e1), e2)
       
-      case Call(e1, e2) if !isValue(e2) => Call(e1, step(e2))
+      
+      case Call(e1, e2) if !isValue(e2) => e1 match{
+        case Function(_, _, _) => Call(e1, step(e2))
+        case _ => throw new DynamicTypeError(e)
+      } 
+      
+      
+      
       
       case _ => throw new DynamicTypeError(e)
+      //case _ => throw new UnsupportedOperationException
     }
   }
   
-  /* maybe we should include this somewhere? */
   def iterateStep(e: Expr): Expr =
     if (isValue(e)) e else iterateStep(step(e))
     
